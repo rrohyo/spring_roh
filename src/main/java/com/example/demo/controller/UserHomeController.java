a4a1c7738e68ea915036783973131a8c3b7c997c
rrohyo <aoslwjs2926@gmail.com>
2025-07-29 21:35:03 +0900

    url로 count 변수 컨트롤

--- src/main/java/com/example/demo/controller/UserHomeController.java
+++ src/main/java/com/example/demo/controller/UserHomeController.java
@@ -6,13 +6,30 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UserHomeController {
+	int count = 0;
 	
 	
-	@RequestMapping("/user/home/main")
+	@RequestMapping("/user/home/setCountValue")
 	@ResponseBody
-	public String showMain() {
-		return "Hello World! 안녕하세요";
+	public String setCountValue(int value) {
+		this.count = value;
+		return "count값 " + value + "(으)로 설정";
 	}
+	
+	@RequestMapping("/user/home/getCount")
+	@ResponseBody
+	public String getCount() {
+		return "count = " + this.count;
+	}
+	
+	@RequestMapping("/user/home/setCount")
+	@ResponseBody
+	public String setCount() {
+		this.count = 0;
+		return "count값 초기화";
+	}
+	
+	
 
 	
 

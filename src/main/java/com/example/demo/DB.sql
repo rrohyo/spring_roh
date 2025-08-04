3d9f27eda0416f1cf3c74c000e3bc7046e65a889
rrohyo <aoslwjs2926@gmail.com>
2025-08-04 21:17:18 +0900

    DB스키마 업데이트

--- src/main/java/com/example/demo/DB.sql
+++ src/main/java/com/example/demo/DB.sql
@@ -1,6 +1,6 @@
-DROP DATABASE `spring_2025_08`;
-CREATE DATABASE `spring_2025_08`;
-USE `spring_2025_08`;
+DROP DATABASE `jdbc_db`;
+CREATE DATABASE `jdbc_db`;
+USE `jdbc_db`;
 
 # 게시글 테이블 생성
 CREATE TABLE `article` (
@@ -18,10 +18,13 @@ CREATE TABLE `member` (
     `updateDate` DATETIME NOT NULL,
     `loginId` CHAR(100) NOT NULL,
     `loginPw` CHAR(100) NOT NULL,
+    `authLevel` SMALLINT(2) UNSIGNED DEFAULT 3 COMMENT '권한 레벨 (3=일반, 7=관리자)',
     `name` CHAR(100) NOT NULL,
-    `nickname` char(20) not null,
-    `cellphoneNum` char(20) not null,
-    `email` char(20) not null
+    `nickname` CHAR(20) NOT NULL,
+    `cellphoneNum` CHAR(20) NOT NULL,
+    `email` CHAR(20) NOT NULL,
+    `delStatus` TINYINT(1) UNSIGNED DEFAULT 0 COMMENT '탈퇴 여부(0=탈퇴 전, 1=탈퇴 후)',
+    `delDate` DATETIME COMMENT '탈퇴 날짜'
 );
 
 
@@ -34,12 +37,12 @@ INSERT INTO `article`
 SET `regDate` = NOW(),
     `updateDate` = NOW(),
     `title` = '제목2',
-    `body` = '내용2';
+    `body` = '내용2'; 
 INSERT INTO `article`
 SET `regDate` = NOW(),
     `updateDate` = NOW(),
     `title` = '제목3',
-    `body` = '내용3';
+    `body` = '내용3';    
 
 
 INSERT INTO `member`
@@ -47,28 +50,31 @@ SET `regDate` = NOW(),
     `updateDate` = NOW(),
     `loginId` = 'admin',
     `loginPw` = 'admin',
+    `authLevel` = 7,
     `name` = '관리자',
     `nickname` = '관리자_닉네임',
     `cellphoneNum` = '01045644565',
-    `email` = 'admin@gmail.com';
+    `email` = 'admin@gmail.com'; 
 INSERT INTO `member`
 SET `regDate` = NOW(),
     `updateDate` = NOW(),
     `loginId` = 'test1',
     `loginPw` = 'test1',
+    `authLevel` = 3,
     `name` = '회원1',
     `nickname` = '회원1_닉네임',
     `cellphoneNum` = '01066914545',
-    `email` = '회원1@gmail.com';
+    `email` = '회원1@gmail.com'; 
 INSERT INTO `member`
 SET `regDate` = NOW(),
     `updateDate` = NOW(),
     `loginId` = 'test2',
     `loginPw` = 'test2',
+    `authLevel` = 3,
     `name` = '회원2',
     `nickname` = '회원2_닉네임',
     `cellphoneNum` = '01098765432',
-    `email` = '회원2@gmail.com';
+    `email` = '회원2@gmail.com'; 
 
 SELECT *
 FROM `article`;
